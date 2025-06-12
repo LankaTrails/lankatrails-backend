ffd481ad9cbc97d22212df65d2406d802cd642e3
sakiththewmika <sakiththewmika@gmail.com>
2025-06-12 22:28:51 +0530

    Create springboot project

---
+++ src/main/java/com/lankatrails/lankatrails_backend/LankatrailsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.lankatrails.lankatrails_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LankatrailsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LankatrailsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lankatrails/lankatrails_backend/LankatrailsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lankatrails.lankatrails_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LankatrailsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
