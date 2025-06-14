c54fdc3ba451469b5e6cd95a2c3b34ddc88b12d6
sakiththewmika <sakiththewmika@gmail.com>
2025-06-14 21:00:18 +0530

    [LT-11] Create dto classes

---
+++ src/main/java/com/lankatrails/lankatrails_backend/dtos/request/ProviderRegistrationRequest.java
@@ -0,0 +1,55 @@
+package com.lankatrails.lankatrails_backend.dtos.request;
+
+import com.lankatrails.lankatrails_backend.model.enums.ProviderCategory;
+import com.lankatrails.lankatrails_backend.model.enums.UserRole;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Getter
+@Setter
+public class ProviderRegistrationRequest extends RegistrationRequest {
+    @Size(max = 50)
+    private String businessName;
+
+    @Size(max = 100)
+    private String businessDescription;
+
+    @Size(max = 255)
+    private String logoUrl;
+
+    @Setter
+    @Getter
+    private Set<ProviderCategory> categories;
+
+    public ProviderRegistrationRequest() {
+        this.setUserRole(UserRole.PROVIDER);
+    }
+
+    public @Size(max = 50) String getBusinessName() {
+        return businessName;
+    }
+
+    public void setBusinessName(@Size(max = 50) String businessName) {
+        this.businessName = businessName;
+    }
+
+    public @Size(max = 100) String getBusinessDescription() {
+        return businessDescription;
+    }
+
+    public void setBusinessDescription(@Size(max = 100) String businessDescription) {
+        this.businessDescription = businessDescription;
+    }
+
+    public @Size(max = 255) String getLogoUrl() {
+        return logoUrl;
+    }
+
+    public void setLogoUrl(@Size(max = 255) String logoUrl) {
+        this.logoUrl = logoUrl;
+    }
+
+}
---
+++ src/main/java/com/lankatrails/lankatrails_backend/dtos/response/RegistrationResponse.java
@@ -0,0 +1,19 @@
+package com.lankatrails.lankatrails_backend.dtos.response;
+
+import com.lankatrails.lankatrails_backend.model.enums.UserRole;
+import com.lankatrails.lankatrails_backend.model.enums.UserStatus;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegistrationResponse {
+    private Long userId;
+    private String email;
+    private UserRole role;
+    private UserStatus status;
+}
