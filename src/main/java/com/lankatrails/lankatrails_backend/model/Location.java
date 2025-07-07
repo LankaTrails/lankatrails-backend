1b4f7ea3f96694234c8a1deef54b434aa665c9fe
Pamali Weerasinghe <pamaliweerasinghe@gmail.com>
2025-07-07 12:18:09 +0530

    [LT-22] Added the Location entity

---
+++ src/main/java/com/lankatrails/lankatrails_backend/model/Location.java
@@ -0,0 +1,25 @@
+package com.lankatrails.lankatrails_backend.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "location")
+@Getter
+@Setter
+public class Location {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long locationId;
+    private String formattedAddress;
+    private String city;
+    private String district;
+    private String province;
+    private String country;
+    private String postalCode;
+    private Double latitude;
+    private Double longitude;
+
+
+}
