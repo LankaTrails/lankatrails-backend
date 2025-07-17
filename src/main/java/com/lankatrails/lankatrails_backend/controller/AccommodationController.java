dac54534e6388d773fb1f2a3aa9e95dfb7f6e9ec
Pamali Weerasinghe <pamaliweerasinghe@gmail.com>
2025-07-17 15:03:08 +0530

    [LT-22] Added accommodation service insertion

---
+++ src/main/java/com/lankatrails/lankatrails_backend/controller/AccommodationController.java
@@ -0,0 +1,53 @@
+package com.lankatrails.lankatrails_backend.controller;
+
+import com.lankatrails.lankatrails_backend.dtos.request.AccommodationServiceRequestDTO;
+import com.lankatrails.lankatrails_backend.dtos.response.APIResponse;
+import com.lankatrails.lankatrails_backend.service.AccommodationService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api")
+public class AccommodationController {
+
+    @Autowired
+    AccommodationService accommodationService;
+
+    @PostMapping(value = "/provider/accommodation/add", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<APIResponse<String>> addTransport
+            (
+                    @RequestPart("service") @Valid AccommodationServiceRequestDTO service,
+                    @RequestPart(value = "images", required = false) List<MultipartFile> images,
+                    BindingResult result
+            ){
+        if (result.hasErrors()){
+            Map<String,String> errors = new HashMap<>();
+            result.getFieldErrors().forEach(field ->{
+                errors.put(field.getField(), field.getDefaultMessage());
+            });
+            APIResponse<String> errorResponse = APIResponse.<String>builder()
+                    .success(false)
+                    .message("Validation Failed")
+                    .details(errors)
+                    .build();
+            return  new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+        }else{
+            APIResponse<String> ActivityServiceDTO =  accommodationService.addService(service, images);
+            return new ResponseEntity<>(ActivityServiceDTO,HttpStatus.CREATED);
+        }
+
+    }
+}
---
+++ src/main/java/com/lankatrails/lankatrails_backend/service/impl/AccommodationServiceImpl.java
@@ -0,0 +1,94 @@
+package com.lankatrails.lankatrails_backend.service.impl;
+
+import com.lankatrails.lankatrails_backend.dtos.request.AccommodationServiceRequestDTO;
+import com.lankatrails.lankatrails_backend.dtos.request.ActivityServiceRequest;
+import com.lankatrails.lankatrails_backend.dtos.request.PolicySectionRequest;
+import com.lankatrails.lankatrails_backend.dtos.request.TabSectionRequest;
+import com.lankatrails.lankatrails_backend.dtos.response.APIResponse;
+import com.lankatrails.lankatrails_backend.exception.ResourceNotFoundException;
+import com.lankatrails.lankatrails_backend.exception.ServiceAlreadyExistsException;
+import com.lankatrails.lankatrails_backend.model.Accommodation;
+import com.lankatrails.lankatrails_backend.model.ActivityService;
+import com.lankatrails.lankatrails_backend.model.Category;
+import com.lankatrails.lankatrails_backend.model.Provider;
+import com.lankatrails.lankatrails_backend.model.enums.ServiceCategory;
+import com.lankatrails.lankatrails_backend.repositories.AccommodationRepository;
+import com.lankatrails.lankatrails_backend.repositories.CategoryRepository;
+import com.lankatrails.lankatrails_backend.security.utils.AuthUtils;
+import com.lankatrails.lankatrails_backend.service.AccommodationService;
+import com.lankatrails.lankatrails_backend.service.ImageService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class AccommodationServiceImpl implements  AccommodationService {
+    @Autowired
+    CategoryRepository categoryRepository;
+
+    @Autowired
+    AccommodationRepository accommodationRepository;
+
+    @Autowired
+    TabsImpl tabsImpl;
+
+    @Autowired
+    PolicyImpl policyImpl;
+
+    @Autowired
+    private ImageService imageService;
+
+    @Autowired
+    ModelMapper modelMapper;
+
+    @Autowired
+    AuthUtils authUtils;
+
+
+
+    @Override
+    @Transactional
+    public APIResponse<String> addService(AccommodationServiceRequestDTO services, List<MultipartFile> images) {
+        Category category = categoryRepository.findByCategoryName(ServiceCategory.ACCOMMODATION)
+                .orElseThrow(() -> new ResourceNotFoundException("Category", 4L));
+
+        Accommodation mappedObj = modelMapper.map(services, Accommodation.class);
+        mappedObj.setCategory(category);
+
+        Provider provider = (Provider) authUtils.loggedInUser();
+        mappedObj.setProvider(provider);
+
+        Optional<Accommodation> checkDb = accommodationRepository.findByServiceName(mappedObj.getServiceName());
+        Accommodation lastServiceAdded;
+
+        if (checkDb.isEmpty()) {
+            // Save the base service object first
+            lastServiceAdded = accommodationRepository.save(mappedObj);
+
+            // Set Tabs
+            List<TabSectionRequest> tabsReq = services.getTabsSection();
+            tabsImpl.addTabs(tabsReq, lastServiceAdded);
+
+            // Set Policies
+            List<PolicySectionRequest> policyReq = services.getPolicySection();
+//            Boolean policyAdditionStatus = policyImpl.addPolicies(policyReq, lastServiceAdded, category);
+
+            // Upload and associate images
+            imageService.uploadImagesForService(images, lastServiceAdded);
+
+        } else {
+            throw new ServiceAlreadyExistsException(checkDb.get().getServiceId());
+        }
+
+        return APIResponse.<String>builder()
+                .success(true)
+                .message("Service Added Successfully")
+                .data("")
+                .build();
+    }
+}
