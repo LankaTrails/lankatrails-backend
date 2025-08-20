3eb0c7df73427ce794ec0f8c30aa04b9ad891fab
Himesh-kaveesha <himeshkaveesha984@gmail.com>
2025-08-21 00:56:53 +0530

    review and rating dto's

---
+++ src/main/java/com/lankatrails/lankatrails_backend/dtos/request/RateAndReviewDTO.java
@@ -0,0 +1,20 @@
+package com.lankatrails.lankatrails_backend.dtos.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+public class RateAndReviewDTO {
+
+    private Long id;               // review_id
+    private LocalDateTime createdDate;
+    private Long rate;
+    private String review;
+
+    private Long serviceId;           // keep service id simple
+    private TouristSummaryDTO tourist; // who wrote the review
+
+  }
