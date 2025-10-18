ffe748a8b53c6af1342caeaf9994aac2ca843a2c
Pamali Weerasinghe <pamaliweerasinghe@gmail.com>
2025-10-18 12:27:23 +0530

    [LT-79] Altered the complaint license section

--- src/main/java/com/lankatrails/lankatrails_backend/dtos/request/BusinessDetailDTO.java
+++ src/main/java/com/lankatrails/lankatrails_backend/dtos/request/BusinessDetailDTO.java
@@ -1,9 +1,10 @@
-package com.lankatrails.lankatrails_backend.dtos.response;
+package com.lankatrails.lankatrails_backend.dtos.request;
 
-import com.lankatrails.lankatrails_backend.dtos.request.ContactPersonDTO;
 import com.lankatrails.lankatrails_backend.model.enums.BusinessType;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.List;
 
 @Getter
 @Setter
@@ -13,4 +14,6 @@ public class BusinessDetailDTO {
     private String businessRegistrationNumber;
     private String businessRegistrationUrl;
     private ContactPersonDTO contactPerson;
+    private String categoryName;
+    private List<LicenseDTO> licenseDTOList;
 }
--- src/main/java/com/lankatrails/lankatrails_backend/service/impl/ProviderServiceImpl.java
+++ src/main/java/com/lankatrails/lankatrails_backend/service/impl/ProviderServiceImpl.java
@@ -3,7 +3,9 @@ package com.lankatrails.lankatrails_backend.service.impl;
 import com.lankatrails.lankatrails_backend.dtos.request.ContactPersonDTO;
 import com.lankatrails.lankatrails_backend.dtos.request.LicenseDTO;
 import com.lankatrails.lankatrails_backend.dtos.response.APIResponse;
-import com.lankatrails.lankatrails_backend.dtos.response.BusinessDetailDTO;
+import com.lankatrails.lankatrails_backend.dtos.request.BusinessDetailDTO;
+import com.lankatrails.lankatrails_backend.dtos.response.BusinessResponseDTO;
+import com.lankatrails.lankatrails_backend.dtos.response.LicenseResponseDTO;
 import com.lankatrails.lankatrails_backend.exception.ResourceNotFoundException;
 import com.lankatrails.lankatrails_backend.model.Category;
 import com.lankatrails.lankatrails_backend.model.License;
@@ -18,9 +20,12 @@ import com.lankatrails.lankatrails_backend.service.ProviderService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProviderServiceImpl implements ProviderService {
@@ -40,14 +45,14 @@ public class ProviderServiceImpl implements ProviderService {
     private LicenseRepository licenseRepository;
 
     @Override
-    public APIResponse<BusinessDetailDTO> getBusinessDetails() {
+    public APIResponse<BusinessResponseDTO> getBusinessDetails() {
         BusinessDetailDTO businessDetail = new BusinessDetailDTO();
         // Assuming the provider is fetched from the AuthUtils
         Provider provider = providerRepository.findById(authUtils.loggedInUserId())
                 .orElseThrow(() -> new RuntimeException("Provider not found with id: " + authUtils.loggedInUserId()));
 
         if (provider == null) {
-            return APIResponse.<BusinessDetailDTO>builder()
+            return APIResponse.<BusinessResponseDTO>builder()
                     .success(false)
                     .message("Provider not found")
                     .build();
@@ -60,13 +65,70 @@ public class ProviderServiceImpl implements ProviderService {
         businessDetail.setBusinessRegistrationUrl(provider.getBusinessRegistrationUrl());
         businessDetail.setContactPerson(modelMapper.map(provider.getContactPerson(), ContactPersonDTO.class));
 
-        return APIResponse.<BusinessDetailDTO>builder()
+
+        List<License> licenses = licenseRepository.findByProvider_UserId(authUtils.loggedInUserId());
+//                .orElseThrow(()->new ResourceNotFoundException("License", authUtils.loggedInUserId()));
+
+        List<LicenseDTO> licenseDTOList = new ArrayList<>();
+        for (License license:licenses){
+            LicenseDTO licenseDTO = new LicenseDTO();
+            licenseDTO.setLicenseUrl(license.getLicenseUrl());
+            licenseDTO.setCategory(license.getCategory().getCategoryName());
+            licenseDTOList.add(licenseDTO);
+        }
+        businessDetail.setLicenseDTOList(licenseDTOList);
+
+        BusinessResponseDTO businessResponseDTO =new BusinessResponseDTO();
+        businessResponseDTO.setContent(businessDetail);
+
+        return APIResponse.<BusinessResponseDTO>builder()
                 .success(true)
                 .message("Business details retrieved successfully")
-                .data(businessDetail)
+                .data(businessResponseDTO)
                 .build();
     }
-
+//    @Override
+//    public APIResponse<BusinessResponseDTO> getBusinessDetails2() {
+//        BusinessDetailDTO businessDetail = new BusinessDetailDTO();
+//        // Assuming the provider is fetched from the AuthUtils
+//        Provider provider = providerRepository.findById(authUtils.loggedInUserId())
+//                .orElseThrow(() -> new RuntimeException("Provider not found with id: " + authUtils.loggedInUserId()));
+//
+////        if (provider == null) {
+////            return APIResponse.<BusinessDetailDTO>builder()
+////                    .success(false)
+////                    .message("Provider not found")
+////                    .build();
+////        }
+//
+//        // Populate business details from the provider
+//        businessDetail.setProviderId(provider.getUserId());
+//        businessDetail.setBusinessType(provider.getBusinessType());
+//        businessDetail.setBusinessRegistrationNumber(provider.getBusinessRegistrationNumber());
+//        businessDetail.setBusinessRegistrationUrl(provider.getBusinessRegistrationUrl());
+//        businessDetail.setContactPerson(modelMapper.map(provider.getContactPerson(), ContactPersonDTO.class));
+//
+//        List<License> licenses = licenseRepository.findByProvider_UserId(authUtils.loggedInUserId());
+////                .orElseThrow(()->new ResourceNotFoundException("License", authUtils.loggedInUserId()));
+//
+//        List<LicenseDTO> licenseDTOList = new ArrayList<>();
+//        for (License license:licenses){
+//            LicenseDTO licenseDTO = new LicenseDTO();
+//            licenseDTO.setLicenseUrl(license.getLicenseUrl());
+//            licenseDTO.setCategory(license.getCategory().getCategoryName());
+//            licenseDTOList.add(licenseDTO);
+//        }
+//        businessDetail.setLicenseDTOList(licenseDTOList);
+//
+//        BusinessResponseDTO businessResponseDTO =new BusinessResponseDTO();
+//        businessResponseDTO.setContent(businessDetail);
+//
+//        return APIResponse.<BusinessResponseDTO>builder()
+//                .success(true)
+//                .message("Business details retrieved successfully")
+//                .data(businessResponseDTO)
+//                .build();
+//    }
     @Override
     public APIResponse<String> licenseRenewal(List<LicenseDTO> licenseDTO, List<MultipartFile> licenseFiles) {
         Provider provider = providerRepository.findByUserId(authUtils.loggedInUserId()).orElseThrow(()->new ResourceNotFoundException("Provider",authUtils.loggedInUserId()));
@@ -115,4 +177,121 @@ public class ProviderServiceImpl implements ProviderService {
 
 
     }
+
+    @Override
+    @Transactional
+    public APIResponse<LicenseResponseDTO> getLicenses() {
+        List<License> licenses = licenseRepository.findAll();
+        List<LicenseDTO> licenseDTOList = new ArrayList<>();
+        for(License license:licenses){
+            LicenseDTO licenseDTO = new LicenseDTO();
+
+//            licenseDTO.setCategoryName(license.getCategory().getCategoryName().getDisplayName());
+
+            if(license.getCategory().getCategoryName() ==ServiceCategory.ACTIVITY){
+                if(license.getProvider().getActivityApprovalStatus() == ApprovalStatus.PENDING||
+                        license.getProvider().getActivityApprovalStatus() == ApprovalStatus.RENEWAL){
+                    licenseDTO.setBusinessName(license.getProvider().getBusinessName());
+                    licenseDTO.setCategoryName("Activity");
+                    licenseDTO.setStatus(license.getProvider().getActivityApprovalStatus());
+                    licenseDTO.setLicenseNumber(license.getLicenseNumber());
+                    licenseDTO.setBusinessName(license.getProvider().getBusinessName());
+                    licenseDTO.setExpiryDate(license.getExpiryDate());
+                    licenseDTO.setLicenseId(license.getLicenseId());
+                    licenseDTOList.add(licenseDTO);
+                }
+            }
+           if (license.getCategory().getCategoryName()==ServiceCategory.ACCOMMODATION){
+               if(license.getProvider().getAccommodationApprovalStatus() == ApprovalStatus.PENDING||
+                       license.getProvider().getAccommodationApprovalStatus() == ApprovalStatus.RENEWAL){
+                   licenseDTO.setBusinessName(license.getProvider().getBusinessName());
+                   licenseDTO.setCategoryName("Accommodation");
+                   licenseDTO.setStatus(license.getProvider().getAccommodationApprovalStatus());
+                   licenseDTO.setLicenseNumber(license.getLicenseNumber());
+                   licenseDTO.setBusinessName(license.getProvider().getBusinessName());
+                   licenseDTO.setExpiryDate(license.getExpiryDate());
+                   licenseDTO.setLicenseId(license.getLicenseId());
+                   licenseDTOList.add(licenseDTO);
+               }
+           }
+
+           if (license.getCategory().getCategoryName()==ServiceCategory.FOOD_BEVERAGE){
+               if(license.getProvider().getFoodApprovalStatus() == ApprovalStatus.PENDING||
+                       license.getProvider().getFoodApprovalStatus() == ApprovalStatus.RENEWAL){
+                   licenseDTO.setBusinessName(license.getProvider().getBusinessName());
+                   licenseDTO.setCategoryName("Food & Beverage");
+                   licenseDTO.setStatus(license.getProvider().getFoodApprovalStatus());
+                   licenseDTO.setLicenseNumber(license.getLicenseNumber());
+                   licenseDTO.setBusinessName(license.getProvider().getBusinessName());
+                   licenseDTO.setExpiryDate(license.getExpiryDate());
+                   licenseDTO.setLicenseId(license.getLicenseId());
+                   licenseDTOList.add(licenseDTO);
+               }
+           }
+
+           if (license.getCategory().getCategoryName()==ServiceCategory.TOUR_GUIDE){
+               if(license.getProvider().getTourGuideApprovalStatus() == ApprovalStatus.PENDING||
+                       license.getProvider().getTourGuideApprovalStatus() == ApprovalStatus.RENEWAL){
+                   licenseDTO.setBusinessName(license.getProvider().getBusinessName());
+                   licenseDTO.setCategoryName("Tour Guide");
+                   licenseDTO.setStatus(license.getProvider().getTourGuideApprovalStatus());
+                   licenseDTO.setLicenseNumber(license.getLicenseNumber());
+                   licenseDTO.setBusinessName(license.getProvider().getBusinessName());
+                   licenseDTO.setExpiryDate(license.getExpiryDate());
+                   licenseDTO.setLicenseId(license.getLicenseId());
+                   licenseDTOList.add(licenseDTO);
+               }
+           }
+
+           if (license.getCategory().getCategoryName()==ServiceCategory.TRANSPORT){
+               if(license.getProvider().getTransportApprovalStatus() == ApprovalStatus.PENDING||
+                       license.getProvider().getTransportApprovalStatus() == ApprovalStatus.RENEWAL){
+                   licenseDTO.setBusinessName(license.getProvider().getBusinessName());
+                   licenseDTO.setCategoryName("Transport");
+                   licenseDTO.setStatus(license.getProvider().getTransportApprovalStatus());
+                   licenseDTO.setLicenseNumber(license.getLicenseNumber());
+                   licenseDTO.setBusinessName(license.getProvider().getBusinessName());
+                   licenseDTO.setExpiryDate(license.getExpiryDate());
+                   licenseDTO.setLicenseId(license.getLicenseId());
+                   licenseDTOList.add(licenseDTO);
+               }
+           }
+
+
+
+
+
+//            if (license.getCategory().getCategoryName() == ServiceCategory.ACTIVITY){
+//                licenseDTO.setCategoryName("Activity");
+//                //check the approval status from the database
+//            }else if(license.getCategory().getCategoryName() == ServiceCategory.ACCOMMODATION){
+//                licenseDTO.setCategoryName("Accommodation");
+//            }else if(license.getCategory().getCategoryName() == ServiceCategory.TOUR_GUIDE){
+//                licenseDTO.setCategoryName("Tour Guide");
+//            }else if (license.getCategory().getCategoryName() == ServiceCategory.TRANSPORT){
+//                licenseDTO.setCategoryName("Transport");
+//            }else if (license.getCategory().getCategoryName() == ServiceCategory.FOOD_BEVERAGE){
+//                licenseDTO.setCategoryName("Food & Beverage");
+//            }
+
+
+        }
+        LicenseResponseDTO licenseResponseDTO = new LicenseResponseDTO();
+        licenseResponseDTO.setContent(licenseDTOList);
+        return APIResponse.<LicenseResponseDTO>builder()
+                .success(true)
+                .message("Loaded licenses successfully")
+                .data(licenseResponseDTO)
+                .build();
+    }
+
+    @Override
+    public APIResponse<LicenseResponseDTO> getLicense(Long id) {
+        License license = licenseRepository.findById(id)
+                .orElseThrow(()->new ResourceNotFoundException("License",id));
+        //find the provider details
+        return null;
+    }
+
+
 }
